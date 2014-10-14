7cd74ce25b104ae32fb323ed3c1f85547d4ea70c
Jackon Yang <jiekunyang@gmail.com>
2014-10-14 08:02:36 +0800

    append dispname

--- rebuild_Readme.sh
+++ rebuild_Readme.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+function AppendReadme {  # md5, file_name, filesize
+    # | sequence | md5 | file_name  | Size |
+}
+
 function GetFileName_json {
     if [ ! -e "$1" ]
     then
@@ -21,13 +25,50 @@ function GetFileName_json {
     echo $filename
 }
 
-function GetFileName_Readme {
+function AppendDispName_Json {
     if [ ! -e "$1" ]
     then
+        echo file "$1" not exists
         return 0
     fi
 
-    echo $filename
+    dispname=`cat "$1" | jq '.dispname'`
+    if [ null = "$dispname" ]
+    then
+        file_md5=`echo "$1" | awk 'BEGIN{FS="."} {print $1}'`
+        file_size=`du "$file_md5".pdf | awk '{print $1}'`
+
+        echo "| 0000 | "$dispname".pdf | $(GetSizeInNiceString $filesize) | $file_md5 | " >> README.md
+        git add README.md
+        git commit -m"Append dispname $dispname"
+    fi
+}
+
+
+
+
+function AppendName_Json {
+    if [ ! -e "$1" ]
+    then
+        echo file "$1" not exists
+        return 0
+    fi
+
+    filename=`cat "$1" | jq '.dispname'`
+    if [ null = "$filename" ]
+    then
+        echo "$filename"
+    fi
+
+    return 0
+
+    num=$((`cat "$1" | jq '.rawname' | wc -l` - 2 ))
+    echo $num
+
+    if [ $num -gt 1 ]
+    then
+        echo "$1" `cat "$1" | jq '.rawname'` >> multi.json
+    fi
 }
 
 function FilterMultiLine {
@@ -42,24 +83,28 @@ function RetrieveLastLine {
         head -10 README.md >> README.md.temp
         tail -1 README.md >> README.md.temp
         mv README.md.temp README.md
+        git add README.md
+        git commit -m'RetrieveLastLine'
     fi
 }
 
 git stash
 
+i=1
+remain=`git branch | wc -l`
+
 for branch_name in `git branch | grep b_`
 do
+    echo "===== Searching Seq-$i ==== $remain remaining ====="
     git checkout $branch_name >> /dev/null
     git checkout .
-    content=`tail -n +11 README.md`
-    if [ `echo "$content" | wc -l` -gt 2 ]
-    then
-        #FilterMultiLine
-        RetrieveLastLine
-        git add README.md
-        git commit -m'RetrieveLastLine'
-    fi
+    #FilterMultiLine
+    # RetrieveLastLine
+    AppendName_Json *.json
+
+    i=$((i + 1 ))
+    remain=$((remain - 1 ))
 done
 
 git checkout dev_add
-git stash pop
+# git stash pop
